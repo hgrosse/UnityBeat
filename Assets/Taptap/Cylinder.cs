3d5306e0c8906d908932651b0316554ba9096c50
thibseisel <https://github.com/thibseisel/rest.git>
2016-11-28 16:10:20 +0100

    Initial commit

---
+++ Assets/Taptap/Cylinder.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cylinder : MonoBehaviour {
+    Texture texture1;
+    Texture texture2;
+
+    // Use this for initialization
+    void Start () {
+        texture1 = (Texture)Resources.Load("renard_01");
+        texture2 = (Texture)Resources.Load("renard_02");
+        GetComponent<Renderer>().material.mainTexture = texture1;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void OnTriggerEnter(Collider other) {
+        System.Console.WriteLine("Boum");
+        //GetComponent<Renderer>().material.mainTexture = texture2;
+        Destroy(this);
+
+    }
+}
---
+++ Assets/Taptap/TempoSphere.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class TempoSphere : MonoBehaviour {
+
+    public GameObject sphere;
+
+	// Use this for initialization
+	void Start () {
+        transform.Translate(0, 1, 40);
+        GameObject instanciatedObject = (GameObject) Instantiate(sphere, transform.position, transform.rotation);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
