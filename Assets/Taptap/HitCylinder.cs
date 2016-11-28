6b5bb0822e36d1efd22f4c0e5feb12ef7501a151
thibseisel <https://github.com/thibseisel/rest.git>
2016-11-28 18:01:48 +0100

    Collision detection between 4 cylinders and the player arms

---
+++ Assets/Taptap/HitCylinder.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HitCylinder : MonoBehaviour {
+
+    public Color defaultColor;
+    public Color pressedColor;
+
+    private Renderer mRenderer;
+
+    // Use this for initialization
+    void Start () {
+        mRenderer = GetComponent<Renderer>();
+        toggleColor(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void OnTriggerEnter(Collider other) {
+        toggleColor(true);
+    }
+
+    void OnTriggerExit(Collider other) {
+        toggleColor(false);
+    }
+
+    private void toggleColor(bool pressed) {
+        mRenderer.material.SetColor("_Color", pressed ? pressedColor : defaultColor);
+    }
+}
